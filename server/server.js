3e88c499f0cdda30a03751de774f367dc7f9805a
NguyenDonLam <donlamn@student.unimelb.edu.au>
2024-11-11 15:28:41 +1100

    feat: Peer server connection

--- server/server.js
+++ server/server.js
@@ -7,8 +7,6 @@ const io = require("socket.io")(server);
 
 const { v4: uuidV4} = require("uuid");
 
-server.listen(3000)
-
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.get("/", (req, res) => {
@@ -21,6 +19,9 @@ app.get("/:room", (req, res) => {
 
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId) => {
-    socket.join(roomId)
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
   })
 });
+
+server.listen(3000);
