b19bbdd1a0855ef2943b8a54d99a8e64a18d448b
NguyenDonLam <donlamn@student.unimelb.edu.au>
2024-11-11 14:55:14 +1100

    feat: add server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { v4 } from "uuid";
+
 function App() {
   return (
-    <>
-      Hello
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${v4()}`} />} />
+        <Route path="/documents/:id" element={<div />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
